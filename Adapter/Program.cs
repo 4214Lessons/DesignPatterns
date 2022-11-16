cd227085b9e8dec2b16cfce32de93547bb78ec7c
Tural Novruzov <novruzov_t@itstep.edu.az>
2022-11-16 17:51:13 +0400

    Add Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Adapter;
+
+interface IAudioFile
+{
+    void Play();
+}
+
+class Mp3 : IAudioFile
+{
+    public void Play()
+    {
+        Console.WriteLine("Mp3 audio playing");
+    }
+}
+
+class Wav : IAudioFile
+{
+    public void Play()
+    {
+        Console.WriteLine("Wav audio playing");
+    }
+}
+
+class FLAC : IAudioFile
+{
+    public void Play()
+    {
+        Console.WriteLine("FLAC audio playing");
+    }
+}
+
+
+
+// Nuget
+class OGG
+{
+    public void PlayAudio()
+    {
+        Console.WriteLine("OGG audio playing");
+    }
+}
+
+
+
+
+
+// Object Adapter
+class OggAdapter : IAudioFile
+{
+    private readonly OGG ogg;
+
+    public OggAdapter()
+    {
+        ogg = new OGG();
+    }
+
+    public void Play()
+    {
+        ogg.PlayAudio();
+    }
+}
+
+
+
+
+
+//// Class Adapter
+// class OggAdapter : OGG, IAudioFile
+// {
+//     public void Play()
+//     {
+//         PlayAudio();
+//     }
+// }
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        List<IAudioFile> winamp = new()
+        {
+            new Mp3(),
+            new OggAdapter(),
+            new Wav(),
+            new FLAC(),
+            new OggAdapter(),
+            new Mp3(),
+            new FLAC(),
+        };
+
+
+        foreach (var audio in winamp)
+            audio.Play();
+    }
+}
