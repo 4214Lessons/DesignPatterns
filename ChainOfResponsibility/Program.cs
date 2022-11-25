b98d718051eafdd2f99dbbdd764a6cba0936d804
novruzov_t <novruzov_t@STHQ0127-01>
2022-11-25 17:14:09 +0400

    add CoR

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ChainOfResponsibility;
+
+
+abstract class CompilerCoR
+{
+    protected CompilerCoR? Next { get; set; }
+
+    public virtual CompilerCoR SetNext(CompilerCoR next)
+    {
+        Next = next;
+        return this;
+    }
+
+    public abstract void Handle();
+}
+
+
+class SyntaxAnalyzer : CompilerCoR
+{
+    public override void Handle()
+    {
+        Console.WriteLine("SyntaxAnalyzer");
+        Next?.Handle();
+    }
+}
+
+class LexicalAnalyzer : CompilerCoR
+{
+    public override void Handle()
+    {
+        Console.WriteLine("LexicalAnalyzer");
+        Next?.Handle();
+    }
+}
+
+class Linker : CompilerCoR
+{
+    public override void Handle()
+    {
+        Console.WriteLine("Linker");
+        Next?.Handle();
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        CompilerCoR compiler = new SyntaxAnalyzer()
+            .SetNext(new LexicalAnalyzer()
+            .SetNext(new Linker()));
+
+        compiler.Handle();
+    }
+}
