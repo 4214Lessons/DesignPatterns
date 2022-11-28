429e95c3f47aa0bb9e3daedd84100616d25e0068
Tural Novruzov <novruzov_t@itstep.edu.az>
2022-11-28 17:43:21 +0400

    Add State

---
+++ State/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace State; // Vəziyyət
+
+interface IState
+{
+    void Do(Television tv);
+}
+
+
+class Television
+{
+    public IState State { get; set; }
+
+    public Television()
+    {
+        State = new OffState();
+    }
+
+    public void PressToggleButton() => State.Do(this);
+}
+
+
+class OffState : IState
+{
+    public void Do(Television tv)
+    {
+        tv.State = new OnState();
+        Console.WriteLine("Televizor açıldı");
+    }
+}
+
+class OnState : IState
+{
+    public void Do(Television tv)
+    {
+        tv.State = new OffState();
+        Console.WriteLine("Televizor bağlandı");
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Television tv = new Television();
+
+        tv.PressToggleButton();
+        tv.PressToggleButton();
+        tv.PressToggleButton();
+        tv.PressToggleButton();
+    }
+}
