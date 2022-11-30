5f93b9cd66bb9eeca315f4f3c5d99fbd9801e4f8
Tural Novruzov <novruzov_t@itstep.edu.az>
2022-11-30 15:42:15 +0400

    Add Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Strategy; // Strategiya
+
+
+interface ISerializer
+{
+    void Serialize();
+}
+
+
+
+class JsonSerializer : ISerializer
+{
+    public void Serialize()
+    {
+        Console.WriteLine("Json Serialize");
+    }
+}
+
+class XMLSerializer : ISerializer
+{
+    public void Serialize()
+    {
+        Console.WriteLine("XML Serialize");
+    }
+}
+
+class BinarySerializer : ISerializer
+{
+    public void Serialize()
+    {
+        Console.WriteLine("Binary Serialize");
+    }
+}
+
+
+
+class Context
+{
+    private ISerializer _serializer;
+
+  
+    public void SetStrategy(ISerializer serializer)
+    {
+        _serializer = serializer;
+    }
+
+    public void Serialize()
+    {
+        _serializer.Serialize();
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Context _context = new Context();
+
+        // _context.SetStrategy(new BinarySerializer());
+        // _context.SetStrategy(new JsonSerializer());
+        // _context.SetStrategy(new XMLSerializer());
+
+        _context.Serialize();
+    }
+}
